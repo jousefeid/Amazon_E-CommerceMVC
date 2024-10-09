f970595d0f96ce63428f149c22ec4285515992d1
Youssef Eid <120427993+jousefeid@users.noreply.github.com>
2024-10-09 22:44:45 +0300

    file Add

--- TestCoreApp/obj/Debug/net6.0/TestCoreApp.AssemblyInfo.cs
+++ TestCoreApp/obj/Debug/net6.0/TestCoreApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestCoreApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0f952ae46a886904b83a71102ff3fe6d853929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5e96a766b97da589466bfc564bc0ea160d6c7c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestCoreApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestCoreApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
